ae1cc048c77626ee71f21cb7bd84bfd5eb0a85e2
Kall <infra@kalltech.net>
2018-10-10 12:51:25 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='binance',
-      version='0.4',
-      py_modules=['binance'],
-      description='Binance API wrapper',
-      url='https://github.com/Kalltech/binance',
-      author='Takaki Oshima/Kalltech',
-      author_email='t@takakioshima.com')
